585543555d853491ecf80fc1f5f45fb0a408f0fa
davidfayanju1 <davidfayanju01@gmail.com>
2025-04-18 18:49:59 +0100

    hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <PrimaryLayout>
       <div className="page-container bg-black overflow-hidden mt-[5.6rem] min-h-[47rem]">
         <section className="image-section bg-gray-100 flex items-center justify-center relative h-[50rem]">
-          <div className="text-container absolute inset-0 h-full w-full flex flex-col items-center justify-center">
+          <div className="text-container absolute inset-0 bg-black/20 h-full w-full flex flex-col items-center justify-center">
             <span className="block md:text-[1.4rem] text-[1.1rem] font-bold mx-auto text-center md:w-[50%] w-[80%] text-white mb-[2rem]">
               A statement piece, not an everyday cap. Crafted with precision.
               Released in rarity
@@ -17,7 +17,7 @@ const Home = () => {
             </button>
           </div>
           <img
-            src="/images/hero4.jpg"
+            src="/images/hero5.jpg"
             alt="hero-image"
             className="md:w-[50%] w-[95%] h-full object-cover object-top"
           />
