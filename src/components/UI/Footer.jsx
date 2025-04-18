a8bcc3473455b03ddb533d561a009cfa448960fc
davidfayanju1 <davidfayanju01@gmail.com>
2025-04-18 18:22:47 +0100

    Styling corrections

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="footer-section min-h-[10rem] bg-gray-500">
-      <div className="flex items-center justify-between"></div>
+    <div className="footer-section min-h-[10rem] pt-3 px-5 border-t-[1px] border-gray-200">
+      <div className="flex items-center justify-between">
+        <div className="flex-container">
+          <span className="font-semibold block text-[.9rem] text-black">
+            NEWSLETTER
+          </span>
+
+          <a href="tel:09044937333" className="">
+            Contact Us
+          </a>
+        </div>
+
+        <div className="mega-container">
+          <span className="text-container block text-[.9rem] text-gray-400">
+            &copy; 2025 UPTOWN
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,22 +4,22 @@ import PrimaryLayout from "../layout/PrimaryLayout";
 const Home = () => {
   return (
     <PrimaryLayout>
-      <div className="page-container overflow-hidden mt-[5.6rem] min-h-screen">
-        <section className="image-section bg-white flex items-center justify-center relative h-[40rem]">
+      <div className="page-container bg-black overflow-hidden mt-[5.6rem] min-h-[47rem]">
+        <section className="image-section bg-gray-100 flex items-center justify-center relative h-[50rem]">
           <div className="text-container absolute inset-0 h-full w-full flex flex-col items-center justify-center">
             <span className="block md:text-[1.4rem] text-[1.1rem] font-bold mx-auto text-center md:w-[50%] w-[80%] text-white mb-[2rem]">
               A statement piece, not an everyday cap. Crafted with precision.
               Released in rarity
             </span>
 
-            <button className="rounded-full text-white w-[12rem] border-solid border-white border-[1px] px-2 py-3 flex items-center justify-center text-[.9rem]">
+            <button className="rounded-full text-black w-[12rem] bg-white border-none outline-none px-2 py-3 flex items-center justify-center text-[.9rem]">
               Request Access
             </button>
           </div>
           <img
-            src="/images/hero3.jpg"
+            src="/images/hero4.jpg"
             alt="hero-image"
-            className="md:w-[50%] w-[90%] h-full object-cover object-top"
+            className="md:w-[50%] w-[95%] h-full object-cover object-top"
           />
         </section>
       </div>
