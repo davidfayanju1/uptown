f887776e86faa0b0c1b58b85f9398ef11aad1916
davidfayanju1 <davidfayanju01@gmail.com>
2025-09-07 14:43:23 +0100

    ui updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -101,9 +101,9 @@ const Home = () => {
 
   return (
     <PrimaryLayout>
-      <div className="page-container bg-black overflow-hidden min-h-[calc(100vh-5rem)] relative">
+      <div className="page-container bg-black overflow-hidden md:min-h-[calc(100vh-5rem)] min-h-[calc(90vh-5rem)] relative">
         {/* Slider container with calc for viewport height minus footer */}
-        <div className="slider-container relative w-full h-[calc(100vh-5rem)] overflow-hidden">
+        <div className="slider-container relative w-full md:h-[calc(100vh-5rem)] h-[calc(90vh-5rem)] overflow-hidden">
           {slides.map((slide, index) => (
             <div
               key={index}
